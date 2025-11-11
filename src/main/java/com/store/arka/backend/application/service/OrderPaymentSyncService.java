6e66a32aedf4cd4e1a0a36fe6f717e87329b97b7
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-11-10 23:23:58 -0500

    feat(payment): integrate SecurityUtils to enforce role-based and ownership access control

--- src/main/java/com/store/arka/backend/application/service/OrderPaymentSyncService.java
+++ src/main/java/com/store/arka/backend/application/service/OrderPaymentSyncService.java
@@ -2,6 +2,9 @@ package com.store.arka.backend.application.service;
 
 import com.store.arka.backend.application.port.in.IOrderUseCase;
 import com.store.arka.backend.application.port.out.IOrderPaymentSyncPort;
+import com.store.arka.backend.domain.enums.OrderStatus;
+import com.store.arka.backend.domain.model.Payment;
+import com.store.arka.backend.shared.security.SecurityUtils;
 import com.store.arka.backend.shared.util.ValidateAttributesUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,18 +17,21 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderPaymentSyncService implements IOrderPaymentSyncPort {
   private final IOrderUseCase orderUseCase;
+  private final SecurityUtils securityUtils;
 
   @Override
   public void markOrderPaid(UUID orderId) {
     ValidateAttributesUtils.throwIfIdNull(orderId, "Order ID");
     orderUseCase.payOrderById(orderId);
-    log.info("[ORDER_PAYMENT_SYNC_SERVICE][MARK_ORDER_PAID] Order ID {} was marked as paid", orderId);
+    log.info("[ORDER_PAYMENT_SYNC_SERVICE][MARK_ORDER_PAID] User(id={}) has marked the Order(id={}) whit status=({})",
+        securityUtils.getCurrentUserId(), orderId, OrderStatus.PAID);
   }
 
   @Override
   public void markOrderCanceled(UUID orderId) {
     ValidateAttributesUtils.throwIfIdNull(orderId, "Order ID");
     orderUseCase.cancelOrderById(orderId);
-    log.info("[ORDER_PAYMENT_SYNC_SERVICE][MARK_ORDER_CANCELED] Order ID {} was marked as cancelled", orderId);
+    log.info("[ORDER_PAYMENT_SYNC_SERVICE][MARK_ORDER_CANCELED] User(id={}) has marked the Order(id={}) whit status=({})",
+        securityUtils.getCurrentUserId(), orderId, OrderStatus.CANCELED);
   }
 }
